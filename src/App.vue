dc0619e0e669ebcfdeb9b34f5ed70828be2b940e
leezii <leezii.atsh@outlook.com>
2017-09-19 22:05:05 +0800

    save a bug

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,30 @@
 
 <script>
   export default {
-    created () {
-      this.$http.post('http://192.168.1.203:9002/image/vc/valid', {
-        keyCode: '111',
-        imgCode: '111'
-      }).then(({data}) => {
+    methods: {
+      async test () {
+        let {data} = await this.$http.post('http://192.168.1.203:9002/image/vc/valid', {
+          keyCode: '111',
+          imgCode: '111'
+        })
         console.log(data)
-      })
+      }
+    },
+    created () {
+//      this.$http.post('http://192.168.1.203:9002/image/vc/valid', {
+//        keyCode: '111',
+//        imgCode: '111'
+//      }).then(({data}) => {
+//        console.log(data)
+//      }).catch(err => {
+//        console.log(err)
+//        throw err
+//      })
+      this.test()
       let a = [1, 2, 2, 2]
+      let [d, ...c] = a
+      console.log(c)
+      console.log(d)
       let b = a.map(v => v)
       console.log(Object.assign(a, b))
     }
