10f9ca6e1c78663f2ea11b1e6bb4cb04c99fe70d
leezii <leezii.atsh@outlook.com>
2017-12-02 09:23:29 +0800

    update webpacl

--- build/env.js
+++ build/env.js
@@ -1,15 +1,17 @@
 /**
  * Created: leezii
- * Date: 2017/10/24
- * Time: 22:11
+ * Date: 2017/10/30
+ * Time: 12:17
  */
 module.exports = {
   development: {
     env: 'development',
     buildTime: +new Date(),
+    upload: 'https://upload.qbox.me'
   },
   production: {
     env: 'production',
     buildTime: +new Date(),
+    upload: 'https://upload.qbox.me'
   }
 }
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,10 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const BundleAnalyzerPlugin = require(
+  'webpack-bundle-analyzer').BundleAnalyzerPlugin
+const HtmlWebpackIncludeAssetsPlugin = require(
+  'html-webpack-include-assets-plugin')
 const CUR_ENV = require('./env')[process.env.NODE_ENV]
 const isBundle = CUR_ENV.env === 'production'
 process.noDeprecation = true
-module.exports = {
+let config = {
   entry: {
     main: './src/main.js'
   },
@@ -36,44 +41,19 @@ module.exports = {
           fallback: 'style-loader',
           use: 'css-loader!less-loader'
         })
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'fonts/[name].[ext]?[hash:3]'
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'images/[name].[ext]?[hash:3]'
-        }
       }
     ]
-  },
-  externals: {
-    'vue': 'Vue',
-    'vue-router': 'VueRouter',
-    'element-ui': 'ELEMENT',
-    'babel-polyfill': '_babelPolyfill',
-    'echarts': 'echarts',
-    'axios': 'axios',
-    'vuex': 'Vuex'
   },
   devtool: isBundle ? false : '#cheap-module-eval-source-map',
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      'common': path.join(__dirname, '../src/common'),
-      'components': path.join(__dirname, '../src/components'),
-      'javascripts': path.join(__dirname, '../src/statics/javascripts'),
-      'less': path.join(__dirname, '../src/statics/less'),
-      'layout': path.join(__dirname, '../src/layout'),
-      'page': path.join(__dirname, '../src/page')
+      vue$: 'vue/dist/vue.esm.js',
+      common: path.join(__dirname, '../src/common'),
+      layout: path.join(__dirname, '../src/layout'),
+      page: path.join(__dirname, '../src/page'),
+      javascripts: path.join(__dirname, '../src/statics/javascripts'),
+      less: path.join(__dirname, '../src/statics/less'),
+      statics: path.join(__dirname, '../src/statics')
     },
     extensions: ['.js', '.vue']
   },
@@ -87,33 +67,91 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '*': {
-        target: 'https://test.ezhangyu.com',
+        target: 'http://192.168.1.202',
         changeOrigin: true,
         secure: false
       }
     }
   },
   plugins: [
-    new CleanWebpackPlugin('dist',
-      {
-        root: path.resolve(__dirname, '../'),
-        verbose: true,
-        dry: false
-      }),
     new HtmlWebpackPlugin({
       favicon: 'favicon.ico',
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      minify: { // 压缩HTML文件
-        removeComments: true, // 移除HTML中的注释
-        collapseWhitespace: true // 删除空白符与换行符
+      minify: {
+        //鍘嬬缉HTML鏂囦欢
+        removeComments: true, //绉婚櫎HTML涓殑娉ㄩ噴
+        collapseWhitespace: true //鍒犻櫎绌虹櫧绗︿笌鎹㈣绗�
       }
     }),
     new webpack.DefinePlugin({
       CUR_ENV: JSON.stringify(CUR_ENV)
     }),
-    new ExtractTextPlugin('style/styles.css'),
-    new UglifyJSPlugin()
+    new ExtractTextPlugin('style/styles.css')
   ]
 }
+if (isBundle) {
+  config.plugins.push(
+    new CleanWebpackPlugin('dist', {
+      root: path.resolve(__dirname, '../'),
+      verbose: true,
+      dry: false
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    }),
+    new UglifyJSPlugin(),
+    new CopyWebpackPlugin([
+      // {
+      //   from: './ie',
+      //   to: 'ie'
+      // },
+      // {
+      //   from: './404.html'
+      // }
+    ]),
+    new HtmlWebpackIncludeAssetsPlugin({
+      assets: [
+        {
+          path: '//at.alicdn.com/t/font_372074_ong22op7y76cg14i.css',
+          type: 'css'
+        },
+        {
+          path: '//unpkg.com/babel-polyfill@6.26.0/dist/polyfill.min.js',
+          type: 'js'
+        },
+        {
+          path: '//unpkg.com/vue@2.5.3/dist/vue.min.js',
+          type: 'js'
+        },
+        {
+          path: '//cdn.bootcss.com/axios/0.16.2/axios.min.js',
+          type: 'js'
+        },
+        {
+          path: '//unpkg.com/vuex@3.0.1/dist/vuex.min.js',
+          type: 'js'
+        },
+        {
+          path: '//unpkg.com/vue-router@3.0.1/dist/vue-router.min.js',
+          type: 'js'
+        },
+        {
+          path: '//unpkg.com/element-ui@2.0.2/lib/index.js',
+          type: 'js'
+        },
+      ],
+      append: false,
+      publicPath: ''
+    }))
+  config.externals = {
+    'vue': 'Vue',
+    'vue-router': 'VueRouter',
+    'element-ui': 'ELEMENT',
+    'babel-polyfill': '_babelPolyfill',
+    'axios': 'axios',
+    'vuex': 'Vuex'
+  }
+}
+module.exports = config
